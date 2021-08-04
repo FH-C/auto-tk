db62e4efd7edb13362a9e71a90bd250f5bda2eab
fh <fh@qq.com>
2021-08-04 17:03:21 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import sys
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 from crawler import crawl
 from utils import init_logger
 
 if __name__ == '__main__':
     init_logger()
-    crawl()
+    if len(sys.argv) == 1:
+        scheduler = BlockingScheduler()
+        scheduler.add_job(crawl, 'cron', day_of_week='0', hour=10, minute=0)
+        scheduler.start()
+    else:
+        crawl()
