7c61ec89e59ac86b2bf794da073b8009e7211a8a
fh <fh@qq.com>
2021-08-05 10:59:56 +0800

    default

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ import os
 class Settings():
     USERNAME = os.getenv('USERNAME', '')
     PASSWORD = os.getenv('PASSWORD', '')
-    PUBLIC_KEY = os.getenv('PUBLIC_KEY', '')
+    PUBLIC_KEY = os.getenv('PUBLIC_KEY', 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKf9iZkA5HEFw4zt7MRBkcmgUiz5+r5eqDOKbaurEbScmXd3ZZTtyzirqkYKRIH5mQ+8hq+Wd/pTZNXHS8L0+88CAwEAAQ==')
     MAX_TRIES = os.getenv('MAX_TRIES', 5)
 
 
